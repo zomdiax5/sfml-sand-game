396fb7ff97805d14fdaef77681fb999e17684012
zomdiax5 <zomdiax5games@gmail.com>
2021-11-09 18:21:18 +0100

    Englishified the linux compilation script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-g++ -ggdb3 -I . -I ./profiler *.cpp profiler/*.cpp -std=c++17 -pthread -lstdc++ -lsfml-graphics -lsfml-window -lsfml-system -Wall -o SandGame.86_64
-echo "\n Tutaj będzie output programu: \n\n"
-./SandGame.86_64
-echo "\n\n\n To okno zniknie za 0s...\n"
+g++ -ggdb3 -I . -I ./profiler *.cpp profiler/*.cpp -std=c++17 -pthread -lstdc++ -lsfml-graphics -lsfml-window -lsfml-system -Wall -o SandGame.x86_64
+echo "Here be program output:"
+./SandGame.x86_64
+echo "The program exited."
