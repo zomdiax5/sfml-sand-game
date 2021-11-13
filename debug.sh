eed3db837a5c35f1d08fbdf1362d708ff654c218
zomdiax5 <zomdiax5games@gmail.com>
2021-11-13 04:34:16 +0100

    yeas, more warnings

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
-g++ -g -I . -I ./profiler *.cpp profiler/*.cpp -std=c++17 -pthread -lstdc++ -lsfml-graphics -lsfml-window -lsfml-system -Wall -o SandGame.x86_64
+g++ -g -I . -I ./profiler *.cpp profiler/*.cpp -std=c++17 -pthread -lstdc++ -lsfml-graphics -lsfml-window -lsfml-system -Wall -Wextra -Weffc++  -o SandGame.x86_64
 echo "Here be program output:"
 ./SandGame.x86_64
 echo "The program exited."
